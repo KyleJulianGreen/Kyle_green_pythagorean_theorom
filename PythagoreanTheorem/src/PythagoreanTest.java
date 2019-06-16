f7cd04a91e735dfc879385f5bf7b8e62e6e7cf67
KyleJulianGreen <50314541+KyleJulianGreen@users.noreply.github.com>
2019-06-16 17:22:52 -0500

    Delete PythagoreanTest.java

--- PythagoreanTheorem/src/PythagoreanTest.java
+++
@@ -1,8 +0,0 @@
-public class PythagoreanTest {
-    public static void main (String[] args){
-        Pythagorean iD = new Pythagorean();
-        double Hypotenuse;
-        Hypotenuse = iD.calculateHypotenuse(4, 5);
-        System.out.println(Hypotenuse);
-    }
-}
